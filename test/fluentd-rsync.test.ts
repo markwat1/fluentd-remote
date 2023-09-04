c3f5f8f9a7fc6ba96b21e35ace93763048aa1802
mark <mark@watana.be>
2023-09-05 06:11:09 +0900

    Initial commit

---
+++ test/fluentd-rsync.test.ts
@@ -0,0 +1,17 @@
+// import * as cdk from 'aws-cdk-lib';
+// import { Template } from 'aws-cdk-lib/assertions';
+// import * as FluentdRsync from '../lib/fluentd-rsync-stack';
+
+// example test. To run these tests, uncomment this file along with the
+// example resource in lib/fluentd-rsync-stack.ts
+test('SQS Queue Created', () => {
+//   const app = new cdk.App();
+//     // WHEN
+//   const stack = new FluentdRsync.FluentdRsyncStack(app, 'MyTestStack');
+//     // THEN
+//   const template = Template.fromStack(stack);
+
+//   template.hasResourceProperties('AWS::SQS::Queue', {
+//     VisibilityTimeout: 300
+//   });
+});
